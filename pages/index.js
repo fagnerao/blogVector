acec19c3d79ccfeadced4cc6e45def64c26b0a3b
Fagner Araújo <fagnersi@gmail.com>
2022-12-30 14:02:11 -0300

    mudanças no header

--- pages/index.js
+++ pages/index.js
@@ -89,8 +89,6 @@ export async function loadPosts() {
     method: 'GET',
     headers: { 
       "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "Authorization", 
-      "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE" ,
       "Content-Type": "application/json;charset=UTF-8"   
      },
 };
